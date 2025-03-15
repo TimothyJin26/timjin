ba2dd32cb9d692d84fa0b5ea4e0e10e204eabff2
Timothy Jin <timj26@student.ubc.ca>
2025-03-15 11:42:04 -0700

    Match styling to figma, remove awful drop shadow, add custom fonts

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,18 +3,29 @@ export default function Hero() {
         <>
             <div className="h-screen flex flex-col justify-center items-center">
                 <div className="w-5/6 mx-auto">
-                    <div className="text-5xl font-bold text-[#301000] pb-3 drop-shadow-[2.0px_2.0px_0.3px_rgba(0,0,0,0.1)]">
-                        So glad you stopped by!
+                    <div className="text-5xl font-bold text-[#301000] pb-3">
+                        Glad you stopped by!
                     </div>
-                    <div className="text-3xl font-semibold text-[#633f3d] pb-3 drop-shadow-[1.0px_1.0px_1.0px_rgba(0,0,0,0.0)]">
+                    <div className="text-3xl font-semibold text-[#633f3d] pb-16">
                         Here's a little about me
                     </div>
-                    <div>
+                    <div className="pb-8">
                         I'm Tim, a third year Computer Science student at the
                         University of British Columbia. I'm passionate about
+                        <br />
                         leveraging technology to drive transformative change in
-                        the world in areas like entertainment, healthcare, and
-                        beyond through software development and AI!
+                        the world in areas like entertainment, healthcare,
+                        <br />
+                        productivity and beyond through software development and
+                        AI!
+                    </div>
+                    <div>
+                        When I'm not coding, you can find me skiing the local
+                        mountains, going on hikes, learning a new song on the
+                        piano,
+                        <br />
+                        or exploring the world, trying to find the tastiest
+                        foods and the best spots to capture the perfect photo.
                     </div>
                 </div>
             </div>
--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -2,14 +2,14 @@ import { NavLink } from "react-router-dom";
 
 export default function Navbar() {
     return (
-        <nav className="fixed top-0 z-50 w-full bg-[#d9efff]">
-            <div className="mx-10 flex items-center justify-between py-6">
+        <nav className="fixed top-0 z-50 w-full bg-[#FFFDFA]">
+            <div className="mx-10 flex items-center justify-between py-8">
                 <NavLink to="/">
                     <button className="text-gray-700 hover:text-black text-2xl">
                         timjin
                     </button>
                 </NavLink>
-                <div className="flex items-center space-x-10 mr-20 font-medium">
+                <div className="flex font-inria text-lg items-center space-x-16 mr-16 font-medium">
                     <NavLink to="/projects">
                         <button className="text-gray-700 hover:text-black">
                             Projects
