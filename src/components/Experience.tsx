6c7a740f775e8595cbf6b50e036eb71d554cc37a
Timothy Jin <timj26@student.ubc.ca>
2025-04-29 09:35:04 -0700

    Update padding on experience page, add locations to details

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -4,8 +4,8 @@ export default function Experience() {
     return (
         <>
             <div className="h-screen w-screen flex justify-center items-center">
-                <div className="w-5/6 ml-28">
-                    <div className="text-5xl font-bold text-[#301000] pb-32">
+                <div className="w-5/6">
+                    <div className="text-5xl font-bold text-[#301000] pt-10 pb-16 pl-16">
                         Experience
                     </div>
                     <Timeline />
--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -14,7 +14,7 @@ export default function Timeline() {
             id: 2,
             label: "Stanford Emergency Medicine",
             role: "Student Researcher",
-            date: "May 2024 - Present",
+            date: "May 2024 - Present | Palo Alto - Remote",
             details:
                 "Developing and benchmarking ML models used to analyze live ED-data.\nBuilding and maintaining AWS infrastructure used to transport, analyze, and feed data to our frontend dashboard.\nExpanding our UI to support new metrics and analytics on live patient data.",
         },
@@ -22,7 +22,7 @@ export default function Timeline() {
             id: 3,
             label: "Rivian",
             role: "Software Engineer Intern",
-            date: "May 2024 - Dec 2024",
+            date: "May 2024 - Dec 2024 | Vancouver BC",
             details:
                 "Created various tools to aid and accelrate the integration of mobile app features.\nThese tools include a Python based vehicle simulator,  a Google Protocol Buffer message publishing framework, and a React, Typescript, and AWS based vehicle simulator dashboard.\nDuring my time here, I also lead the integration of various mobile app features including the 2024 Halloween feature, overseeing development and testing.",
         },
@@ -30,7 +30,7 @@ export default function Timeline() {
             id: 4,
             label: "UBC Cloud Innovation Centre",
             role: "Software Developer Co-op",
-            date: "Jan 2023 - Aug 2023",
+            date: "Jan 2023 - Aug 2023 | Vancouver BC",
             details:
                 "Developed cloud-based healthcare projects to help physical rehabilitation and Parkinson's patients, researchers, and doctors.\nBuilt multiple Flutter apps to collect data to feed to our AWS supported backends for ML analysis, data processing, and storage.\nDesigned and developed multiple accessible mobile apps to display analytics and metrics for patients and doctors.",
         },
