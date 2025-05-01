eedcb75d3b7b663e6275add3e0ccd39e4f32309f
Timothy Jin <timj26@student.ubc.ca>
2025-04-30 21:06:47 -0700

    Reduce space between title and timeline in experience page

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -5,7 +5,7 @@ export default function Experience() {
         <>
             <div className="h-screen w-full flex flex-col justify-center items-center overflow-x-hidden">
                 <div className="w-5/6">
-                    <div className="text-5xl font-bold text-[#301000] pt-10 pb-24 pl-28">
+                    <div className="text-5xl font-bold text-[#301000] pt-12 pb-16 pl-28">
                         Experience
                     </div>
                     <Timeline />
