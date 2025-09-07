b871c5e438564db2a217e3bb261465e809f852d1
Timothy Jin <Timothy.jin26@gmail.com>
2025-09-07 14:10:32 -0700

    Fix spacing in about description

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -15,22 +15,17 @@ export default function About() {
                         <div className="text-lg text-black leading-8">
                             <div className="pb-8">
                                 I'm Tim, a fourth year Computer Science student
-                                at the University of British Columbia. I'm
-                                passionate about
-                                <br />
-                                leveraging technology to drive transformative
-                                change in the world in areas like entertainment,
-                                healthcare,
-                                <br />
-                                productivity and beyond through software
-                                development and AI!
+                                at the University of British Columbia. 
+                            </div>
+                            <div className="pb-8">
+                                I'm passionate about leveraging technology to drive transformative
+                                change in the world in areas like entertainment, healthcare,
+                                productivity and beyond through software development and AI!
                             </div>
                             <div>
                                 When I'm not coding, you can find me skiing the
                                 local mountains, going on hikes, learning a new
-                                song on the piano,
-                                <br />
-                                or exploring the world, trying to find the
+                                song on the piano, or exploring the world, trying to find the
                                 tastiest foods and the best spots to capture the
                                 perfect photo.
                             </div>
